16e669cc43baa2b59e7c2b240328404cdb3cff36
Oakesso <olivierakesso@gmail.com>
2021-08-27 01:28:45 +0200

    modify read questions section 2

--- client/src/components/Forms/ReadForm.jsx
+++ client/src/components/Forms/ReadForm.jsx
@@ -33,39 +33,59 @@ class ReadForm extends React.Component {
             count: this.state.count - 1,
         });
     };
+
+    handleNothing = () => {
+        console.log("Nothing here !", this.state.count)
+    };
     
     render() {
         const { allQuestions } = this.state;
-        console.log("this.state A: ", this.state);
+        // console.log("this.state A: ", this.state);
+        // console.log("this.state.count : ", this.state.count);
+        
         // at 1rst state.allQuestion is undefined.
         // we want defined values.
-        if (this.state.allQuestions.length !== 0) {
-            console.log("this.state.allQuestions A: ", this.state.allQuestions);
-            console.log("this.state.allQuestions[1]._id A: ", this.state.allQuestions[1]._id);
-            console.log('this.state.count A: ', this.state.count)
-        return (
-            <div>  
-                Test 1 : 
-                <button onClick= {this.handleDecrement}>previous</button>
-                <button onClick= {this.handleIncrement}>next</button>
-                <p>{this.state.count}</p>               
-            </div>
-        )            
-        } else {
-        return (
-            <div> 
-                Test 2 :  
-                <button onClick= {this.handleDecrement}>previous</button>
-                <button onClick= {this.handleIncrement}>next</button>
-                <p>{this.state.count}</p>                
-            </div>
-            )      
-        }
+        if (this.state.count === -1 || this.state.count === this.state.allQuestions.length) {
+            if (this.state.count <= -1) {
+                console.log("this.state.count : ", this.state.count);
+                this.state.count = -1; 
+                return (
+                    <div>  
+                        <div>--- </div>
+                        <button onClick= {this.handleNothing}>previous</button>
+                        <button onClick= {this.handleIncrement}>next</button>
+                        <p>---</p>
+                    </div>
+                );
+            }
+            if(this.state.count >= this.state.allQuestions.length) {
+                this.state.count = this.state.allQuestions.length;
+                return (
+                    <div>  
+                        <div>--- </div>
+                        <button onClick= {this.handleDecrement}>previous</button>
+                        <button onClick= {this.handleNothing}>next</button>
+                        <p>---</p>
+                    </div>
+                );
+            }            
+        } else  if (this.state.allQuestions.length !== 0) {
+            // console.log("this.state.allQuestions A: ", this.state.allQuestions);
+            // console.log("this.state.allQuestions[1]._id A: ", this.state.allQuestions[1]._id);
+            // console.log('this.state.count A: ', this.state.count)
+            return (
+                <div>  
+                    <div>Q { this.state.count }  : { this.state.allQuestions[this.state.count].question }</div>
+                    <button onClick= {this.handleDecrement}>previous</button>
+                    <button onClick= {this.handleIncrement}>next</button>
+                    <p>{ this.state.count } / { this.state.allQuestions.length - 1}</p>               
+                </div>
+            );            
+        } 
     }
 }
  
 export default ReadForm;
-
 
 
 {/* <ul className="ul-readform">                         
